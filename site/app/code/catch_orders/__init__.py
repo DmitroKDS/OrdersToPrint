fc30059bba74a3bde4c4acc6119ed455310960d6
DmitroKDS <74712259+DmitroKDS@users.noreply.github.com>
2025-12-11 16:46:10 +0200

    Delete site directory

--- site/app/code/catch_orders/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('catch_orders', __name__)
-
-from . import routes
--- site/app/code/generate_canvas/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('generate_canvas', __name__)
-
-from . import routes
